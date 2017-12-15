8d4c5c64fe7edee1df5eec4e88a07d49a5355d99
cicangia <ez32@hotmail.it>
2017-12-15 11:37:00 +0100

    momentaneo

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-            var scroll = 0;
-            $(document).scroll(function() {
-                scroll = $(this).scrollTop();
+    var scroll = 0;
+    $(document).scroll(function () {
+        scroll = $(this).scrollTop();
 
-                if (scroll > 130) {
-                    $(".background-color").css({
-                        "background-color": "rgba(0,0,0,0.8)",
-                        "height":"60px"
-                    });
-                } else {
-                    $(".background-color").css({
-                        "background-color": "transparent",
-                        "height":"108px"
-                    });
-                }
+        if (scroll > 130) {
+            $(".background-color").css({
+                "background-color": "rgba(0,0,0,0.8)",
+                "height": "60px"
             });
- });
+        } else {
+            $(".background-color").css({
+                "background-color": "transparent",
+                "height": "108px"
+            });
+        }
+    });
+
+    $('body').scrollspy({
+        target: '#mynav'
+    });
+
+//    $(document).on('click', 'a[href^="#"]', function (event) {
+//        event.preventDefault();
+//
+//        $('html, body').animate({
+//            scrollTop: $($.attr(this, 'href')).offset().top
+//        }, 1000);
+//    });
+});
